f5ea1647891151225c6236ffa44e0cce0ae41d31
Niwatori401 <CJackson@firemail.cc>
2023-02-22 06:49:31 +0900

    Allow folder paths with spaces

--- INSTALL.bat
+++ INSTALL.bat
@@ -8,15 +8,15 @@ CALL npm install website-scraper --verbose
 CALL npm install website-scraper-puppeteer --verbose
 
 
-SET base_path=%~dp0
+SET "base_path=%~dp0"
 SET "move_to_extrapath=node_modules\website-scraper-puppeteer\lib\"
 SET "move_from_extrapath=util\index.js"
-SET move_to_path=%base_path%%move_to_extrapath%
-SET move_from_path=%base_path%%move_from_extrapath%
+SET "move_to_path=%base_path%%move_to_extrapath%"
+SET "move_from_path=%base_path%%move_from_extrapath%"
 
-MOVE /Y %move_from_path% %move_to_path%
+MOVE /Y "%move_from_path%" "%move_to_path%"
 
-CALL node index.js %~dp0
+CALL node index.js "%base_path%
 
 wget -r -np -nH -P EatTheDungeon/ --wait=0.2 https://bewilderedgames.com/img/
 wget -r -np -nH -P EatTheDungeon/ --wait=0.2 https://bewilderedgames.com/sfx/
